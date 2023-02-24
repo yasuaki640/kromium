3cad017612325bb278c8bf2b8823faa36be7534e
yasuaki640 <yasuaki640@gmail.com>
2023-02-24 23:18:20 +0900

    なんでこんなめんどくさいことやってるんだろう

--- src/test/kotlin/StyleTest.kt
+++ src/test/kotlin/StyleTest.kt
@@ -1,75 +1,24 @@
+import java.io.File
 import kotlin.test.Test
-import kotlin.test.assertEquals
+import kotlin.test.assertIs
 
 internal class StyleTest {
     @Test
     fun `is able to merge html and css to style tree`() {
-        val root = Node(
-            arrayListOf(
-                Node(
-                    arrayListOf(
-                        Node(
-                            arrayListOf(Node(arrayListOf(), Text("Title"))),
-                            Element(ElementData("h1", hashMapOf()))
-                        ),
-                        Node(
-                            arrayListOf(
-                                Node(
-                                    arrayListOf(
-                                        Node(arrayListOf(),Text("Hello ")),
-                                        Node(
-                                            arrayListOf(Node(arrayListOf(), Text("world"))),
-                                            Element(ElementData("em", hashMapOf()))
-                                        ),
-                                        Node(arrayListOf(), Text("!"))
-                                    ),
-                                    Element(ElementData("p", hashMapOf()))
-                                )
-                            ), Element(ElementData("div", hashMapOf("id" to "main", "class" to "test")))
-                        )
-                    ), Element(ElementData("body", hashMapOf()))
-                )
-            ), Element(ElementData("html", hashMapOf()))
-        )
+        val root = domTree()
+        val styleSheet = cssTree()
 
-        assertEquals(root.nodeType, Element(ElementData("html", HashMap())))
+        val styleTree = styleTree(root, styleSheet)
+        assertIs<StyledNode>(styleTree)
+    }
 
-        val body = root.children[0]
-        assertEquals(body.nodeType, Element(ElementData("body", HashMap())))
+    private fun cssTree(): StyleSheet {
+        val css = File("src/test/resources/test.css").readText()
+        return CSSParser(0u, css).parse(css)
+    }
 
-        val text = body.children[0]
-        assertEquals(
-            text.nodeType,
-            Element(ElementData("h1", hashMapOf()))
-        )
-
-        val div = body.children[1]
-        assertEquals(
-            div.nodeType,
-            Element(
-                ElementData(
-                    "div",
-                    hashMapOf(
-                        Pair("id", "main"),
-                        Pair("class", "test")
-                    )
-                )
-            )
-        )
-
-        val p = div.children[0]
-        assertEquals(p.nodeType, Element(ElementData("p", HashMap())))
-
-        val helloTxt = p.children[0]
-        assertEquals(helloTxt.nodeType, Text("Hello "))
-
-        val em = p.children[1]
-        assertEquals(em.nodeType, Element(ElementData("em", HashMap())))
-
-        val worldTxt = em.children[0]
-        assertEquals(worldTxt.nodeType, Text("world"))
-
-        val exclamation = p.children[2]
-        assertEquals(exclamation.nodeType, Text("!"))
+    private fun domTree(): Node {
+        val html = File("src/test/resources/test.html").readText()
+        return parse(html)
     }
 }
